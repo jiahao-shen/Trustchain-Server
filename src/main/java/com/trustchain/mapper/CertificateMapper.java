6be93b6279907c88f871685e775675af3af01ea3
liyiming11 <Liyiming914@buaa.edu.cn>
2024-04-24 16:48:33 +0800

    增加certificate Mapper

---
+++ src/main/java/com/trustchain/mapper/CertificateMapper.java
@@ -0,0 +1,9 @@
+package com.trustchain.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.trustchain.model.entity.Certificate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CertificateMapper extends BaseMapper<Certificate> {
+}
